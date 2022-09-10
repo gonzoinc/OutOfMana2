8565e0cebbd5574b5fb5b686e47d9a94547a1274
Greg <gonzoinc@users.noreply.github.com>
2022-09-10 18:59:31 -0400

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,73 @@
+-- constants
+local playerName = UnitName("player")
+local defaultMpThreshold = 50
+local antiSpamThresholdValue = 20
+
+-- Saved vars (config)
+local mpThreshold -- mp = mana percentage
+
+-- vars
+local antiSpam = false
+local antiSpamThreshold
+
+local version = GetAddOnMetadata("OutOfMana2","Version")
+-- Addon loaded message
+print("|c0003fc07OOM |r[v|c0003fc07" .. version .. "|r] loaded: /mana")
+
+-- Handle Events
+local f = CreateFrame("Frame");
+f:RegisterEvent("ADDON_LOADED")
+f:RegisterEvent("PLAYER_LOGOUT")
+f:RegisterEvent("UNIT_POWER_UPDATE")
+
+function f:OnEvent(event, arg1, arg2)
+    if event == "ADDON_LOADED" and arg1 == "OutOfMana2" then
+        if not OutOfManaDB2 then
+            OutOfManaDB2 = {}
+            OutOfManaDB2.mpThreshold = defaultMpThreshold
+        end
+        mpThreshold = OutOfManaDB2.mpThreshold
+        antiSpamThreshold = mpThreshold + antiSpamThresholdValue
+    end
+    if event == "PLAYER_LOGOUT" then
+        OutOfManaDB2.mpThreshold = mpThreshold
+    end
+    if event == "UNIT_POWER_UPDATE" then
+        local mp = (UnitPower("player", 0) / UnitPowerMax("player")) * 100 -- current player mana percentage
+        if (arg1 == "player") and (arg2 == "MANA") and (mp <= mpThreshold) and (not antiSpam) then          
+            DoEmote("OOM")
+            SendChatMessage(playerName .. " - I have " .. round(mp) .. "% mana!", IsInGroup(LE_PARTY_CATEGORY_INSTANCE) and "INSTANCE_CHAT" or IsInRaid() and "RAID" or "PARTY")
+            antiSpam = true
+        end
+        if (arg1 == "player") and (arg2 == "MANA") and (mp >= antiSpamThreshold) and antiSpam then
+             antiSpam = false 
+        end
+    end
+end
+
+f:SetScript("OnEvent", f.OnEvent)
+
+function round(number)
+  if (number - (number % 0.1)) - (number - (number % 1)) < 0.5 then
+    number = number - (number % 1)
+  else
+    number = (number - (number % 1)) + 1
+  end
+ return number
+end
+
+-- Commands
+SLASH_SAVED1 = "/mana";
+function SlashCmdList.SAVED(msg)
+    if msg ~= "" then 
+        mpThreshold = tonumber(msg)
+        print("Changed limit to: " .. mpThreshold .. "%")      
+        if (mpThreshold + antiSpamThresholdValue > 100) then
+            antiSpamThreshold = 100
+        else
+            antiSpamThreshold = mpThreshold + antiSpamThresholdValue 
+        end
+    else 
+        print("Limit: " .. mpThreshold .. "%" .. " (\"/mana number\" to change)")
+    end
+end
